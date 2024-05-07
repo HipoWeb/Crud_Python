294cbda7ebac01ed5fb35b2abd1a0a95292ccf82
Hipolito Laptop <u20171a057@upc.edu.pe>
2024-05-07 16:24:37 -0500

    modificar fotos

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template, request, redirect
 from flaskext.mysql import MySQL
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -13,20 +14,21 @@ app.config['MYSQL_DATABASE_DB']='sistema'
 #mysql.init_app(app)
 mysql = MySQL(app)
 
+CARPETA=os.path.join('uploads')
+app.config['CARPETA']=CARPETA
+
+
+
 @app.route('/')
 def index():
     sql="SELECT * FROM `empleados`;"
     conn= mysql.connect()
     cursor=conn.cursor()
     cursor.execute(sql)
-
     empleados=cursor.fetchall()
     print(empleados)
-
-
     conn.commit() 
-    cursor.close()     
-
+    cursor.close() 
     return render_template('empleados/index.html',empleados=empleados)
 @app.route('/destroy/<int:id>')
 def destroy(id):
@@ -47,6 +49,38 @@ def edit(id):
     print(empleados)
     return render_template('empleados/edit.html', empleados=empleados)
 
+@app.route('/update', methods=['POST'])
+def update():
+    _nombre=request.form['txtNombre']
+    _correo=request.form['txtCorreo']
+    _foto=request.files['txtFoto']
+    id=request.form['txtID']
+
+    sql="UPDATE empleados SET nombre=%s,correo= %s WHERE id= %s;"
+    datos=(_nombre,_correo,id)
+
+    conn= mysql.connect()
+    cursor=conn.cursor()
+
+    now= datetime.now()
+    tiempo=now.strftime("%Y%H%M%S")
+
+    if _foto.filename!='':
+        nuevoNombreFoto=tiempo+_foto.filename
+        _foto.save("uploads/"+nuevoNombreFoto)
+
+        cursor.execute("SELECT foto FROM empleados WHERE id= %s", id)
+        fila=cursor.fetchall()
+
+        os.remove(os.path.join(app.config['CARPETA'],fila[0][0]))
+        cursor.execute("UPDATE empleados SET foto=%s WHERE id=%s", (nuevoNombreFoto, id))
+        conn.commit()
+
+
+    cursor.execute(sql, datos)
+    conn.commit() 
+    
+    return redirect('/')
 @app.route('/create')
 def create():
     return render_template('empleados/create.html')
@@ -63,13 +97,8 @@ def storage():
         nuevoNombreFoto=tiempo+_foto.filename
         _foto.save("uploads/"+nuevoNombreFoto)
 
-    
-
-
-
-
     sql="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
-    datos=(_nombre,_correo,_foto.filename)
+    datos=(_nombre,_correo,nuevoNombreFoto)
     
     conn= mysql.connect()
     cursor=conn.cursor()
